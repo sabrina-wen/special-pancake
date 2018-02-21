b6ba07f9d4b70fb3d6d2f6a7dd507d7b722dc18b
sabrina-wen <sabrinawn253@gmail.com>
2018-02-20 22:43:35 -0500

    multiply ok i think

--- matrix.py
+++ matrix.py
@@ -11,18 +11,46 @@ def print_matrix( matrix ):
                 print ""
 
 #identity matrix
-def ident( matrix ): 
-    matrix[0][0] = 1
-    matrix[1][1] = 1
-    matrix[2][2] = 1
-    matrix[3][3] = 1
+def ident( matrix ):
+    for r in range(len(matrix)):
+        for c in range(len(matrix[0])):
+            if (r != c):
+                matrix[r][c] = 0
+            else:
+                matrix[r][c] = 1
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+    m2_copy = new_matrix(len(m2), len(m2[0]))
+    # make a copy of m2
+    for i in range(len(m2)):
+        for j in range(len(m2[0])):
+            m2_copy[i][j] = m2[i][j]
+    # init variables
+    curr_col = 0
+    curr_row = 0
+    # convert each col in m2 into a 1d array, dot product w/ each row in m1
+    while (curr_col < len(m2[0])) and curr_row < len(m2):
+        #print "current coordinate is: (" + str(curr_row) + ", " + str(curr_col) + ")"
+        sum = 0
+        temp = []
+        for r in range(len(m2_copy)):
+            for c in range(len(m2_copy[0])):
+                if (c == curr_col):
+                    temp.append(m2_copy[r][c])
+        for i in range(len(temp)):
+            sum += m1[curr_row][i] * temp[i]
+        m2[curr_row][curr_col] = sum
+        #print "curr_col: " + str(curr_col)
+        #print curr_col % len(m2[0])
+        if (curr_col != len(m2[0]) - 1):
+            #print "lol"
+            curr_col = curr_col + 1
+        else:
+            curr_col = 0
+            curr_row = curr_row + 1
 
-
-
+    return m2
 
 def new_matrix(rows = 4, cols = 4):
     m = []
@@ -37,8 +65,22 @@ m = new_matrix()
 print "original matrix: "
 print_matrix(m)
 ident(m)
+m[0][3] = 2
+m[1][3] = 3
+m[2][3] = 4
 print "original matrix --> identity matrix: "
 print_matrix(m)
-r = new_matrix(2,4)
-print "rand matrix"
+r = new_matrix(4,5)
+incr = 0
+for i in range(len(r)):
+    for j in range(len(r[0])):
+        if (i != len(r) - 1):
+            r[i][j] = incr
+            incr = incr + 1
+        else:
+            r[i][j] = 1
+print "m2 matrix:"
 print_matrix(r)
+
+print "multiply m1 x m2"
+print_matrix(matrix_mult(m,r))
